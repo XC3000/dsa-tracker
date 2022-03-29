ee4f02a00e8530afc0b617699625cd8822749d72
XC3000 <richardrozario.rr@gmail.com>
2022-03-29 14:09:10 +0530

    changes:

---
+++ src/utils/theme.js
@@ -0,0 +1,19 @@
+const Theme = {
+  colors: {
+    main: "#00C9A7",
+    mainDark: "#4D8076",
+    mainLight: "#C4FCEF",
+    mainLighter: "#FCF8FF",
+    textColor: "#333",
+    whiteColor: "#fff",
+    shadow: "rgba(0,0,0.2)",
+  },
+
+  mediaQueries: {
+    smallest: `only screen and (max-width: 35em)`,
+    small: `only screen and (max-width: 375em)`,
+    medium: `only screen and (max-width: 56em)`,
+    large: `only screen and (max-width: 80em)`,
+    largest: `only screen and (max-width: 90em)`,
+  },
+};
